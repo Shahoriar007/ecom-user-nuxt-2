0b525184abd46891bc4e10c5f45e8fa1cae0aac1
Shahoriar007 <shahoriar.fahim@gmail.com>
2024-05-01 19:44:36 +0600

    feat: added tag

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
 
+
+    
+
     <main class="main home skeleton-body">
-        
+
+        <head>
+            <!-- Google Tag Manager -->
+            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TZ7FH7BK');</script>
+        <!-- End Google Tag Manager -->
+        </head>
 
         <div class="bg-gray pb-5">
             <div class="container pb-3">
@@ -51,6 +63,14 @@
         <template>
 			<pv-sticky-footer></pv-sticky-footer>
 		</template>
+
+        <body>
+            <!-- Google Tag Manager (noscript) -->
+            <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TZ7FH7BK"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+            <!-- End Google Tag Manager (noscript) -->
+        </body>
+
        
     </main>
 </template>
@@ -228,5 +248,3 @@ export default {
     },
 };
 </script>
-
-
